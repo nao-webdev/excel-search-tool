0e8bda4f9ac0848c80cfba39ca2fa783b34a629b
nae <209189410+nao-webdev@users.noreply.github.com>
2025-05-07 19:19:12 +0900

    結果が見やすいように出力情報を変更  軽微な修正

--- search.py
+++ search.py
@@ -4,6 +4,7 @@ import sys
 import tkinter as tk
 from tkinter import filedialog
 from tkinter.simpledialog import askstring
+from openpyxl.utils import get_column_letter
 
 # メインウィンドウを非表示で初期化する
 root = tk.Tk()
@@ -29,13 +30,13 @@ file_list =[file_name for file_name in file_list if "xlsx" in file_name]
 # excelファイル一つずつ文字検索をかける
 for file in file_list:
     # ファイルパス生成
-    file_path = folder_path +"/" + file
-    # dictに変換
-    dict = pd.read_excel(file_path, sheet_name=None, header=None, na_filter = False, engine='openpyxl')
+    file_path = os.path.join(folder_path, file)
+    # 辞書型に読み込む
+    sheets_dict = pd.read_excel(file_path, sheet_name=None, header=None, na_filter = False, engine='openpyxl')
 
      # シート一つずつ文字検索をかける
-    for key in dict.keys():
-        df_sheet = dict[key]
+    for key in sheets_dict.keys():
+        df_sheet = sheets_dict[key]
 
         # カラムごとに検索をかける
         num_columns = len(df_sheet.columns)
@@ -44,17 +45,18 @@ for file in file_list:
         for number in range(num_columns):
             # contains('')のところに検索したい文字列をいれる
             column_data = df_sheet[df_sheet[number].str.contains(keyword,na = False)]
-            # 当てはまっていれば(indexが0でなければ)行出力
+
+            # 当てはまっていれば(indexが0でなければ)コンソール出力
             if(not len(column_data.index) == 0):
-                print('---------------' + file +'の'+ key + '----------------')
-                print(column_data)
 
-            
-        
-        
+                # 行番号、列番号、セルの値を変数に格納
+                index = column_data.index[0] + 1
+                column = get_column_letter(number + 1)
+                cell_value = df_sheet.at[column_data.index[0], number]
 
-        
-
-
-
-    
+                # 結果を出力
+                print('【ファイル名】' + file)
+                print('【シート名　】' + key)
+                print('【セルの場所】' + str(column) + str(index))
+                print('【セルの値　】' + cell_value)
+                print('--------------------------------------------------')
